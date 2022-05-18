7d97df8f8b6938ddb9075e4f570658d051f3a2fb
Waseem Ahmed <waseema@bbd.co.za>
2022-05-18 16:20:11 +0200

    add property/id endpoint

--- routes/api/property.js
+++ routes/api/property.js
@@ -8,14 +8,14 @@ router.get('/', async (req, res) => {
     res.json(property);
 });
 
-// router.get('/:id', async (req, res) => {
-//     let listingTypes = await query.GetAllListingTypes();
-//     const found = listingTypes.some(listingType => listingType.ListingTypeID === parseInt(req.params.id));
+router.get('/:id', async (req, res) => {
+    let property = await query.GetProperty(req.params.id);
+    const found = property.some(p => p.PropertyID === parseInt(req.params.id));
 
-//     if (found)
-//         res.json(listingTypes.filter(listingType => listingType.ListingTypeID === parseInt(req.params.id)));
-//     else
-//         res.status(400).json( {error: `No propertyType with id of ${req.params.ListingTypeID}`});
-// });
+    if (found)
+        res.json(property.filter(p => p.PropertyID === parseInt(req.params.id)));
+    else
+        res.status(400).json( {error: `No propertyType with id of ${req.params.PropertyID}`});
+});
 
 module.exports = router;
